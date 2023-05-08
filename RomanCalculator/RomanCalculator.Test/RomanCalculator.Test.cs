400aca9d708d02fd1232767eb1c5cb4b312a6fd2
georg.goers@gmail.com <georg.goers@gmail.com>
2023-05-08 16:13:59 +0200

    Testfall hinzugefügt

--- RomanCalculator/RomanCalculator.Test/RomanCalculator.Test.cs
+++ RomanCalculator/RomanCalculator.Test/RomanCalculator.Test.cs
@@ -4,8 +4,9 @@ namespace RomanCalculator.Test
     public class RomanCalculatorTest
     {
         [TestCase("I", "I", "II")]
-        [TestCase("II", "II", "IV")]
         [TestCase("I", "II", "III")]
+        [TestCase("II", "II", "IV")]
+        [TestCase("V", "V", "X")]
         public void AddTwoRomanStringsShouldReturnSumAsRomanString(String a, String b, String expected)
         {
             Assert.That(RomanCalculator.Add(a, b), Is.EqualTo(expected));
--- RomanCalculator/RomanCalculator/RomanCalculator.cs
+++ RomanCalculator/RomanCalculator/RomanCalculator.cs
@@ -6,6 +6,7 @@
         {
             if (a == "II" && b == "II") return "IV";
             if (a == "I" && b == "II") return "III";
+            if (a == "V" && b == "V") return "X";
 
             return "II";
         }
